a1d1073ef3cd3488754429fbf89d83f1e0453aa0
jodarco <darolthj@hotmail.com>
2024-10-27 18:50:50 +0000

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - Imprime el alfabeto en minúsculas seguido de una nueva línea.
+ * print_alphabet - Imprime el alfabeto en minúsculas
+ * seguido de una nueva línea.
  */
 void print_alphabet(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
